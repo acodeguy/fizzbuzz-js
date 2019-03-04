a74f3ee79b059ab98577665b696fdabfd4fcbeef
Andrew <24258342+acodeguy@users.noreply.github.com>
2019-03-04 16:53:11 +0000

    add failing test for testing buzz

--- spec/fizzBuzzSpec.js
+++ spec/fizzBuzzSpec.js
@@ -8,4 +8,8 @@ describe("FizzBuzz", function() {
     expect(fizzBuzz(3)).toEqual('fizz');
   });
 
+  it('returns "buzz" when passed 5', function() {
+    expect(fizzBuzz(5)).toEqual('buzz');
+  });
+
 });
